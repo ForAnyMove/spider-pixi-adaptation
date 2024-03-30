9d8a8faa9d8a97287315860e05b57fd63a223154
ForAnyMove <alex.osadchuk.98@gmail.com>
2024-03-30 16:38:26 +0100

    preview deploy

--- src/index.js
+++ src/index.js
@@ -16,14 +16,6 @@ await app.init({ autoResize: true, resizeTo: window })
 console.log(app.canvas);
 document.body.appendChild(app.canvas)
 
-window.addEventListener('resize', resize);
-
-function resize() {
-  if (window.innerWidth>window.innerHeight) {
-
-  }
-}
-
 // Load textures
 
 await PIXI.Assets.load(`${texturesLink}/Backgrounds/Background_MainMenu.jpg`)
@@ -68,6 +60,10 @@ const subStyle = new PIXI.TextStyle({
   strokeThickness: 1,
 })
 
+// Main scene container
+
+const mainTab = new PIXI.Container()
+
 // Background
 
 let background = PIXI.Sprite.from(`${texturesLink}/Backgrounds/Background_MainMenu.jpg`)
@@ -79,27 +75,27 @@ app.stage.addChild(background)
 
 let logo = PIXI.Sprite.from(`${texturesLink}/Logos/Logo_RU.png`)
 logo.scale = 0.2*sizeKoef
-logo.x = WIDTH/2-logo.width/2;
-logo.y = HEIGHT/2-HEIGHT/100*30-logo.height/2;
-app.stage.addChild(logo)
+logo.position.set(0, logo.height/2)
+logo.anchor.set(0.5);
+mainTab.addChild(logo)
 
 // General text
 
 const generalText = new PIXI.Text('ПАСЬЯНС ПАУЧИХА', generalStyle);
 generalText.scale = 0.6*sizeKoef
-generalText.x = WIDTH/2-generalText.width/2;
-generalText.y = HEIGHT/2+HEIGHT/100*13-generalText.height/2;
-app.stage.addChild(generalText)
+generalText.position.set(0, HEIGHT/2+HEIGHT/100*13)
+generalText.anchor.set(0.5);
+mainTab.addChild(generalText)
 
 // Frames
-
+const footerContainer = new PIXI.Container()
 
 let footer = PIXI.Sprite.from(`${texturesLink}/Frames/Used_Frame_01.png`)
 footer.scale = 0.5*sizeKoef
-footer.x = WIDTH/2-footer.width/2;
-footer.y = HEIGHT/2+HEIGHT/100*55-footer.height/2 + HEIGHT/100*sizeKoef
-footer.height = footer.height + HEIGHT/100*sizeKoef
-app.stage.addChild(footer)
+footer.position.set(0, 30)
+footerContainer.position.set(WIDTH/2, HEIGHT)
+footer.anchor.set(0.5);
+footerContainer.addChild(footer)
 
 // Buttons
 
@@ -107,126 +103,144 @@ app.stage.addChild(footer)
 
 const longGreenBtn1 = new PIXI.Container()
 let longGreenPanel1 = PIXI.Sprite.from(`${texturesLink}/Buttons/Used_Button_2.2.png`)
+longGreenPanel1.anchor.set(0.5);
 longGreenBtn1.addChild(longGreenPanel1)
 
 const longGreenMainText1 = new PIXI.Text('Одна масть', mainStyle);
 longGreenMainText1.scale = 2
-longGreenMainText1.position.set(150, 40)
+longGreenMainText1.position.set(0, -40)
+longGreenMainText1.anchor.set(0.5);
 longGreenBtn1.addChild(longGreenMainText1)
 
 const longGreenSubText1 = new PIXI.Text('новичок', subStyle);
 longGreenSubText1.scale = 1.6
-longGreenSubText1.position.set(200, 100)
+longGreenSubText1.position.set(0, 20)
+longGreenSubText1.anchor.set(0.5);
 longGreenBtn1.addChild(longGreenSubText1)
 
 longGreenBtn1.scale = 0.3*sizeKoef
-longGreenBtn1.x = WIDTH/2-WIDTH/100*20-longGreenBtn1.width/2;
-longGreenBtn1.y = HEIGHT/2+HEIGHT/100*5-longGreenBtn1.height/2;
-app.stage.addChild(longGreenBtn1)
+longGreenBtn1.x = 0-WIDTH/100*20
+longGreenBtn1.y = HEIGHT/2+HEIGHT/100*5
+mainTab.addChild(longGreenBtn1)
 
 // Container for 2 btn
 const longGreenBtn2 = new PIXI.Container()
 let longGreenPanel2 = PIXI.Sprite.from(`${texturesLink}/Buttons/Used_Button_2.2.png`)
+longGreenPanel2.anchor.set(0.5);
 longGreenBtn2.addChild(longGreenPanel2)
 
 const longGreenMainText2 = new PIXI.Text('Две масти', mainStyle);
 longGreenMainText2.scale = 2
-longGreenMainText2.position.set(150, 40)
+longGreenMainText2.position.set(0, -40)
+longGreenMainText2.anchor.set(0.5);
 longGreenBtn2.addChild(longGreenMainText2)
 
 const longGreenSubText2 = new PIXI.Text('новичок', subStyle);
 longGreenSubText2.scale = 1.6
-longGreenSubText2.position.set(200, 100)
+longGreenSubText2.position.set(0, 20)
+longGreenSubText2.anchor.set(0.5);
 longGreenBtn2.addChild(longGreenSubText2)
 
 longGreenBtn2.scale = 0.3*sizeKoef
-longGreenBtn2.x = WIDTH/2-longGreenBtn2.width/2;
-longGreenBtn2.y = HEIGHT/2+HEIGHT/100*5-longGreenBtn2.height/2;
-app.stage.addChild(longGreenBtn2)
+longGreenBtn2.x = 0
+longGreenBtn2.y = HEIGHT/2 + HEIGHT/100*5
+mainTab.addChild(longGreenBtn2)
 
 // Container for 3 btn
 
 const longGreenBtn3 = new PIXI.Container()
 let longGreenPanel3 = PIXI.Sprite.from(`${texturesLink}/Buttons/Used_Button_2.2.png`)
+longGreenPanel3.anchor.set(0.5);
 longGreenBtn3.addChild(longGreenPanel3)
 
 const longGreenMainText3 = new PIXI.Text('Четыре масти', mainStyle);
 longGreenMainText3.scale = 2
-longGreenMainText3.position.set(130, 40)
+longGreenMainText3.position.set(0, -40)
+longGreenMainText3.anchor.set(0.5);
 longGreenBtn3.addChild(longGreenMainText3)
 
 const longGreenSubText3 = new PIXI.Text('новичок', subStyle);
 longGreenSubText3.scale = 1.6
-longGreenSubText3.position.set(200, 100)
+longGreenSubText3.position.set(0, 20)
+longGreenSubText3.anchor.set(0.5);
 longGreenBtn3.addChild(longGreenSubText3)
 
 longGreenBtn3.scale = 0.3*sizeKoef
-longGreenBtn3.x = WIDTH/2+WIDTH/100*20-longGreenBtn3.width/2;
-longGreenBtn3.y = HEIGHT/2+HEIGHT/100*5-longGreenBtn3.height/2;
-app.stage.addChild(longGreenBtn3)
+longGreenBtn3.x = 0+WIDTH/100*20
+longGreenBtn3.y = HEIGHT/2+HEIGHT/100*5
+mainTab.addChild(longGreenBtn3)
 
 // Container for 4 btn
 
 const longGreenBtn4 = new PIXI.Container()
 let longGreenPanel4 = PIXI.Sprite.from(`${texturesLink}/Buttons/Used_Button_2.2.png`)
+longGreenPanel4.anchor.set(0.5);
 longGreenBtn4.addChild(longGreenPanel4)
 
 const longGreenMainText4 = new PIXI.Text('Одна масть', mainStyle);
 longGreenMainText4.scale = 2
-longGreenMainText4.position.set(150, 40)
+longGreenMainText4.position.set(0, -40)
+longGreenMainText4.anchor.set(0.5);
 longGreenBtn4.addChild(longGreenMainText4)
 
 const longGreenSubText4 = new PIXI.Text('новичок', subStyle);
 longGreenSubText4.scale = 1.6
-longGreenSubText4.position.set(200, 100)
+longGreenSubText4.position.set(0, 20)
+longGreenSubText4.anchor.set(0.5);
 longGreenBtn4.addChild(longGreenSubText4)
 
 longGreenBtn4.scale = 0.3*sizeKoef
-longGreenBtn4.x = WIDTH/2-WIDTH/100*20-longGreenBtn4.width/2;
-longGreenBtn4.y = HEIGHT/2+HEIGHT/100*20-longGreenBtn4.height/2;
-app.stage.addChild(longGreenBtn4)
+longGreenBtn4.x = 0-WIDTH/100*20
+longGreenBtn4.y = HEIGHT/2+HEIGHT/100*20
+mainTab.addChild(longGreenBtn4)
 
 // Container for 5 btn
 
 const longGreenBtn5 = new PIXI.Container()
 let longGreenPanel5 = PIXI.Sprite.from(`${texturesLink}/Buttons/Used_Button_2.2.png`)
+longGreenPanel5.anchor.set(0.5);
 longGreenBtn5.addChild(longGreenPanel5)
 
 const longGreenMainText5 = new PIXI.Text('Две масти', mainStyle);
 longGreenMainText5.scale = 2
-longGreenMainText5.position.set(150, 40)
+longGreenMainText5.position.set(0, -40)
+longGreenMainText5.anchor.set(0.5);
 longGreenBtn5.addChild(longGreenMainText5)
 
 const longGreenSubText5 = new PIXI.Text('новичок', subStyle);
 longGreenSubText5.scale = 1.6
-longGreenSubText5.position.set(200, 100)
+longGreenSubText5.position.set(0, 20)
+longGreenSubText5.anchor.set(0.5);
 longGreenBtn5.addChild(longGreenSubText5)
 
 longGreenBtn5.scale = 0.3*sizeKoef
-longGreenBtn5.x = WIDTH/2-longGreenBtn5.width/2;
-longGreenBtn5.y = HEIGHT/2+HEIGHT/100*20-longGreenBtn5.height/2;
-app.stage.addChild(longGreenBtn5)
+longGreenBtn5.x = 0
+longGreenBtn5.y = HEIGHT/2+HEIGHT/100*20
+mainTab.addChild(longGreenBtn5)
 
 // Container for 6 btn
 
 const longGreenBtn6 = new PIXI.Container()
 let longGreenPanel6 = PIXI.Sprite.from(`${texturesLink}/Buttons/Used_Button_2.2.png`)
+longGreenPanel6.anchor.set(0.5);
 longGreenBtn6.addChild(longGreenPanel6)
 
 const longGreenMainText6 = new PIXI.Text('Четыре масти', mainStyle);
 longGreenMainText6.scale = 2
-longGreenMainText6.position.set(130, 40)
+longGreenMainText6.position.set(0, -40)
+longGreenMainText6.anchor.set(0.5);
 longGreenBtn6.addChild(longGreenMainText6)
 
 const longGreenSubText6 = new PIXI.Text('новичок', subStyle);
 longGreenSubText6.scale = 1.6
-longGreenSubText6.position.set(200, 100)
+longGreenSubText6.position.set(0, 20)
+longGreenSubText6.anchor.set(0.5);
 longGreenBtn6.addChild(longGreenSubText6)
 
 longGreenBtn6.scale = 0.3*sizeKoef
-longGreenBtn6.x = WIDTH/2+WIDTH/100*20-longGreenBtn6.width/2;
-longGreenBtn6.y = HEIGHT/2+HEIGHT/100*20-longGreenBtn6.height/2;
-app.stage.addChild(longGreenBtn6)
+longGreenBtn6.x = 0+WIDTH/100*20
+longGreenBtn6.y = HEIGHT/2+HEIGHT/100*20
+mainTab.addChild(longGreenBtn6)
 
 // Container for Profile button
 
@@ -241,8 +255,6 @@ profileIcon.position.set(30, 20)
 
 signInCoinBtn.addChild(profileIcon)
 signInCoinBtn.scale = 0.3*sizeKoef
-signInCoinBtn.x = WIDTH/100*5-signInCoinBtn.width/2;
-signInCoinBtn.y = HEIGHT/100*10-signInCoinBtn.height/2;
 
 app.stage.addChild(signInCoinBtn)
 
@@ -259,8 +271,6 @@ settingsIcon.position.set(30, 20)
 
 settingsCoinBtn.addChild(settingsIcon)
 settingsCoinBtn.scale = 0.3*sizeKoef
-settingsCoinBtn.x = WIDTH - WIDTH/100*5-settingsCoinBtn.width/2;
-settingsCoinBtn.y = HEIGHT/100*10-settingsCoinBtn.height/2;
 
 app.stage.addChild(settingsCoinBtn)
 
@@ -282,9 +292,9 @@ storyModeText.position.set(5, 170)
 storyModeCoinBtn.addChild(storyModeText)
 
 storyModeCoinBtn.scale = 0.3*sizeKoef
-storyModeCoinBtn.x = WIDTH/2 - WIDTH/100*18-storyModeCoinBtn.width/2;
-storyModeCoinBtn.y = HEIGHT - HEIGHT/100*8-storyModeCoinBtn.height/2;
-app.stage.addChild(storyModeCoinBtn)
+storyModeCoinBtn.x = 0 - WIDTH/100*18-storyModeCoinBtn.width/2;
+storyModeCoinBtn.y = 0 - HEIGHT/100*8-storyModeCoinBtn.height/2;
+footerContainer.addChild(storyModeCoinBtn)
 
 // Container for Challenges button
 
@@ -304,9 +314,9 @@ challengeText.position.set(-15, 170)
 challengeCoinBtn.addChild(challengeText)
 
 challengeCoinBtn.scale = 0.3*sizeKoef
-challengeCoinBtn.x = WIDTH/2 - WIDTH/100*6-challengeCoinBtn.width/2;
-challengeCoinBtn.y = HEIGHT - HEIGHT/100*8-challengeCoinBtn.height/2;
-app.stage.addChild(challengeCoinBtn)
+challengeCoinBtn.x = 0 - WIDTH/100*6-challengeCoinBtn.width/2;
+challengeCoinBtn.y = 0 - HEIGHT/100*8-challengeCoinBtn.height/2;
+footerContainer.addChild(challengeCoinBtn)
 
 // Container for Skins button
 
@@ -326,9 +336,9 @@ skinsText.position.set(15, 170)
 skinsCoinBtn.addChild(skinsText)
 
 skinsCoinBtn.scale = 0.3*sizeKoef
-skinsCoinBtn.x = WIDTH/2 + WIDTH/100*6-skinsCoinBtn.width/2;
-skinsCoinBtn.y = HEIGHT - HEIGHT/100*8-skinsCoinBtn.height/2;
-app.stage.addChild(skinsCoinBtn)
+skinsCoinBtn.x = 0 + WIDTH/100*6-skinsCoinBtn.width/2;
+skinsCoinBtn.y = 0 - HEIGHT/100*8-skinsCoinBtn.height/2;
+footerContainer.addChild(skinsCoinBtn)
 
 // Container for Bonuses button
 
@@ -348,12 +358,24 @@ bonusesText.position.set(15, 170)
 bonusesCoinBtn.addChild(bonusesText)
 
 bonusesCoinBtn.scale = 0.3*sizeKoef
-bonusesCoinBtn.x = WIDTH/2 + WIDTH/100*18-bonusesCoinBtn.width/2;
-bonusesCoinBtn.y = HEIGHT - HEIGHT/100*8-bonusesCoinBtn.height/2;
-app.stage.addChild(bonusesCoinBtn)
+bonusesCoinBtn.x = 0 + WIDTH/100*18-bonusesCoinBtn.width/2;
+bonusesCoinBtn.y = 0 - HEIGHT/100*8-bonusesCoinBtn.height/2;
+footerContainer.addChild(bonusesCoinBtn)
+
+
+
+app.stage.addChild(footerContainer)
+app.stage.addChild(mainTab)
 
 // Animations
 
 app.ticker.add((delta) => {
-  logo.scale = 0.2*(window.innerWidth/950)
+  const currentWidth = window.innerWidth
+  const currentHeight = window.innerHeight
+  signInCoinBtn.position.set(currentWidth/100*5-signInCoinBtn.width/2, currentHeight/100*10-signInCoinBtn.height/2)
+  settingsCoinBtn.position.set(currentWidth - currentWidth/100*5-settingsCoinBtn.width/2, currentHeight/100*10-settingsCoinBtn.height/2)
+  mainTab.position.set(currentWidth/2, 0)
+  background.width = currentWidth
+  background.height = currentHeight
+  footerContainer.position.set(currentWidth/2, currentHeight)
 });
